54a244954580362fb03ae1541ddb6dcaffee7d0a
abrentgit <abprojects.ca@gmail.com>
2019-06-13 21:39:35 -0400

    updated dashboard questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,26 +7,35 @@ export class Dashboard extends React.Component {
     return (
       <form>
         <label>
-          <div>How are you feeling today?</div>
+          <div>How are you feeling today overall?</div>
           <select>
             <option value="Down">Down</option>
             <option value="Okay">Okay</option>
             <option value="Good">Feelin' Good</option>
-            <option value="Super">Super Duper</option>
+            <option value="Happy">Happy</option>
           </select>
         </label>
         <label>
-          <div>What self-care practices have you done today?</div>
+          <div>How many hours of sleep did you get last night?</div>
           <select>
-            <option value="Exercise">Exercise</option>
-            <option value="Meditation">Meditation</option>
-            <option value="Eat">Ate Clean</option>
-            <option value="Yoga">Yoga</option>
-            <option value="Sleep">Good Sleep</option>
+            <option value="option1">1-3</option>
+            <option value="option2">4-6</option>
+            <option value="option3">6-8</option>
+            <option value="option4">8 or more</option>
           </select>
+        </label>
+        <label>
+          <div>What self-care if any have you done today?</div>
+          <input type="checkbox" name="selfcare1" value="Yoga"> Yoga<br>
+          <input type="checkbox" name="selfcare2" value="Meditation"> Meditation<br>
+          <input type="checkbox" name="selfcare3" value="Exercise"> Exercise<br>
+          <input type="checkbox" name="selfcare4" value="Ate-Well"> Ate Well<br>
+          <input type="checkbox" name="selfcare7" value="Walk"> Walk <br>
+          <input type="checkbox" name="none" value="None"> None <br>
         </label>
         <label>
           <div>What's on your mind?</div>
+          <textarea />
           <textarea />
         </label>
         <div>
