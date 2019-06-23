68fcd9e219e0a760903e5688a8492a15c0b5f9a9
abrentgit <abprojects.ca@gmail.com>
2019-06-23 16:21:59 -0400

    got getPractices reducer to pass test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,4 +12,3 @@ export const practicesReducer = (state = initialState, action) => {
     }
     return state;
 }
-
--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,28 +1,87 @@
+import { GET_PRACTICES, getPractices } from "../actions";
+import { practicesReducer } from ".";
 
-describe('getPractices', () => {
-    it('should return the initial state', () => {
+// NEED TO IMPLEMENT REDUCER HERE
 
-        const practiceNameOne = 'Yoga';
-        const practiceNameTwo = 'Meditation'
 
-        const practiceOne = {
-            name: practiceNameOne
-        }
-        const practicerTwo = {
-            name: practiceNameTwo
-        }
-        let state = {
-            practices: [practiceOne, practicerTwo]
-        };
-        expect(state).toEqual({
-            practices: [
-                {
-                    name: practiceNameOne
-                },
-                {
-                    name: practiceNameTwo
-                }
-            ]
-        })
-    })
-})
+// describe('should get practices', () => {
+//     it('should return the initial state', () => {
+
+//         const practiceName3One = 'Yoga';
+//         const practiceNameTwo = 'Meditation'
+
+//         const practiceOne = {
+//             name: practiceNameOne
+//         }
+//         const practicerTwo = {
+//             name: practiceNameTwo
+//         }
+//         let state = {
+//             practices: [practiceOne, practicerTwo]
+//         };
+
+//         const entryOne = {
+//             user: 'User One',
+//             mood: 'Awesome',
+//             hours: '8-10',
+//             practices: ['Yoga',
+//                 'Meditation'
+//             ],
+//             content: 'The sun shined today. It was fab.',
+//             date: '2019-11-12'
+//         };
+
+//         const entryTwo = {
+//             user: 'User One',
+//             mood: 'Awesome',
+//             hours: '8-10',
+//             practices: ['Yoga',
+//                 'Meditation'
+//             ],
+//             content: 'The sun shined today. It was fab.',
+//             date: '2019-11-12'
+//         };
+
+//         expect(state).toEqual({
+//             practices: [
+//                 {
+//                     name: practiceNameOne
+//                 },
+//                 {
+//                     name: practiceNameTwo
+//                 }
+//             ]
+//         })
+//     })
+//     it('should return single entry', () => {
+//         expect(state).toEqual({
+
+//         })
+//     })
+
+// })
+
+/// SAMPLE REDUCER TEST
+
+
+describe('practices reducer', () => {
+
+    it('should return current state on unknown action', () => {
+        let currentState = {};
+        const state = practicesReducer(currentState, { type: '_UNKNOWN' });
+        expect(state).toBe(currentState);
+    });
+
+    describe('getPractices', () => {
+        it('should get practices', () => {
+            let state;
+            state = practicesReducer(state, getPractices({ type: 'GET_PRACTICES' }));
+            expect(state).toEqual({
+                practices: []
+            })
+        });
+    });
+});
+
+
+
