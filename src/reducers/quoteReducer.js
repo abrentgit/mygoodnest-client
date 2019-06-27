715600d74b3493b2215d3698e9ba3972b4962f0a
abrentgit <abprojects.ca@gmail.com>
2019-06-26 22:02:50 -0400

    updated with quotes reducer

---
+++ src/reducers/quoteReducer.js
@@ -0,0 +1,7 @@
+const initialState = {};
+
+const quoteReducer = (state = initialState, action) => {
+    return state
+}
+
+export default quoteReducer;
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import projectReducer from './projectReducer';
 import authReducer from './authReducer';
+import quoteReducer from './quoteReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    project: projectReducer
+    project: projectReducer,
+    quotes: quoteReducer
 })
 
 export default rootReducer;
