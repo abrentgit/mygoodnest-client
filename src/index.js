c66447d4b366fe4a6f6b70a035f5c7a0377a4e2c
abrentgit <abprojects.ca@gmail.com>
2019-06-26 21:33:50 -0400

    started reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 // import { Provider } from 'react-redux';
 // import store from './store';
 import App from './components/App';
 // import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+
+const store = createStore();
 
 ReactDOM.render(
   <App />,
---
+++ src/reducers/entryReducer.js
@@ -0,0 +1,44 @@
+// // import { REGISTER_USER } from '../actions/index';
+
+// // const initialState = {
+// //     result: 1,
+// //     role: '',
+// //     name: '',
+// //     email: '',
+// //     password: ''
+// // }
+
+// // const entryTest = {
+// //     mood: 'Good',
+// //     hours: '8-10',
+// //     practices: ['Yoga', 'Meditation'],
+// //     content: 'Today I went sailing',
+// //     date: '2019-12-12'
+// // }
+
+// // export const entryReducer = (state = entryTest, action) => {
+
+// //     if (action.type === ADD_ENTRY) {
+// //         return Object.assign({}, state, {
+// //             entry: state.entry.concat(action.payload)
+// //         });
+// //     }
+// //     return state;
+// // }
+
+// /// DONE 
+// // register user
+
+// export const registerReducer = (state = initialState, action) => {
+
+//     if (action.type === REGISTER_USER) {
+//         return Object.assign({}, state, {
+//             user: state.user.concat(action.payload)
+//         });
+//     }
+//     return state;
+// }
+
+// // store.subscribe(() => {
+// //     console.log('Store updated', store.getState())
+// // })
